1ccdeba63b50846e7b21fdea447493488fc1982c
Øyvin <desarc00@gmail.com>
2014-03-25 22:53:14 +0100

    level 5 solution

---
+++ no.ntnu.oyvinric.tutorialgame.release/src/no/ntnu/oyvinric/tutorialgame/release/test/Test.java
@@ -0,0 +1,7 @@
+package no.ntnu.oyvinric.tutorialgame.release.test;
+
+import no.ntnu.item.arctis.runtime.Block;
+
+public class Test extends Block {
+
+}
---
+++ no.ntnu.oyvinric.tutorialgame.release/src/no/ntnu/oyvinric/tutorialgame/release/test/component/Component.java
@@ -0,0 +1,27 @@
+package no.ntnu.oyvinric.tutorialgame.release.test.component;
+
+import no.ntnu.item.arctis.runtime.Block;
+
+public class Component extends Block {
+
+	public void test1() {
+	}
+
+	public String test2() {
+		return "";
+	}
+
+	public void doSomething() {
+	}
+
+	public void doSomethingElse() {
+	}
+
+	public void commonOperation() {
+	}
+
+	public boolean booleanOp() {
+		return true;
+	}
+
+}
